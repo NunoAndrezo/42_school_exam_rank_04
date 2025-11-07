187b3fe0651528433a3f66c17b3fd5bc78032e75
nuno <nuno@ubuntu-vm.ubuntu-vm.virtualbox.org>
2025-11-07 19:50:07 +0000

    added an improved version of picoshell

--- lv01/ft_popen/ft_popen.c
+++ lv01/ft_popen/ft_popen.c
@@ -1,53 +1,61 @@
 #include <unistd.h>
-#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
+#include <stdlib.h>
 
 int	ft_popen(const char *file, char *const argv[], char type)
 {
-	int pipefd[2];
-	pid_t id;
-
-	if (!file || !argv)
+	int		fd[2];
+	pid_t	pid;
+	
+	if (!file || !argv || !type)
 		return (-1);
-	if (type != 'r' && type != 'w')
+	if (type != 'w' && type != 'r')
 		return (-1);
-	if (pipe(pipefd) == -1)
+	if (pipe(fd) == -1)
 		return (-1);
-	id = fork();
-	if (id == -1)
+	pid = fork();
+	if (pid < 0)
 	{
-		close(pipefd[0]);
-		close(pipefd[1]);
+		close(fd[0]);
+		close(fd[1]);
 		return (-1);
 	}
-	if (id == 0)
+	else if (pid == 0)
 	{
 		if (type == 'r')
 		{
-			close(pipefd[0]);
-			if (dup2(pipefd[1], STDOUT_FILENO) == -1)
-				exit (EXIT_FAILURE);
-			close(pipefd[1]);
+			close(fd[0]);
+			if (dup2(fd[1], STDOUT_FILENO) == -1)
+			{
+				close(fd[1]);
+				exit(1);
+			}
+			close(fd[1]);
 		}
-		else
+		else if (type == 'w')
 		{
-			close(pipefd[1]);
-			if (dup2(pipefd[0], STDIN_FILENO) == -1)
-				exit (EXIT_FAILURE);
-			close (pipefd[0]);
+			close(fd[1]);
+			if (dup2(fd[0], STDIN_FILENO) == -1)
+			{
+				close(fd[0]);
+				exit(1);
+			}
+			close(fd[0]);
 		}
 		execvp(file, argv);
-		exit (EXIT_FAILURE);
+		exit(1);
 	}
 	if (type == 'r')
 	{
-		close(pipefd[1]);
-		return(pipefd[0]);
+		close(fd[1]);
+		return (fd[0]);
 	}
-	if (type == 'w')
+	else if (type == 'w')
 	{
-		close(pipefd[0]);
-		return (pipefd[1]);
+		close(fd[0]);
+		return (fd[1]);
 	}
 	return (-1);
 }
--- lv01/picoshell/picoshell.c
+++ lv01/picoshell/picoshell.c
@@ -1,110 +1,65 @@
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
-#include <stdio.h>
+#include <sys/wait.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-//#define DEBUG
-
-// Function to handle the redirection and file descriptor management for each child
-int handle_redirection(int i, int n, int fds[2], int tmp_fd) 
+int picoshell(char **cmds[])
 {
-	if (i == 0) // First command
-	{
-		dup2(fds[1], STDOUT_FILENO);
-		close(fds[0]);
-		close(fds[1]);
-	}
-	else if (i < n - 1) // Middle commands
-	{
-		dup2(tmp_fd, STDIN_FILENO);
-		dup2(fds[1], STDOUT_FILENO);
-		close(tmp_fd);
-		close(fds[0]);
-		close(fds[1]);
-	}
-	else // Last command
-	{
-		dup2(tmp_fd, STDIN_FILENO);
-		close(tmp_fd);
-	}
-	return 0;
-}
+	int fd[2];
+	int pid;
+	int stat;
+	int pfd = 0;
 
-int execute_in_pipe(char **cmds[], int n) 
-{
-	int fds[2];
-	int tmp_fd = -1;
-	pid_t pid;
-
-	for (int i = 0; i < n; i++) 
+	for (size_t i = 0; cmds[i]; i++)
 	{
-		if (i < n - 1 && pipe(fds) == -1)
+		if (cmds[i + 1]) // not last command
 		{
-			perror("pipe");
-			return -1;
+			if (pipe(fd) < 0)
+				return 1;
+		}
+		else
+		{
+			fd[0] = -1;
+			fd[1] = -1;
 		}
 		pid = fork();
-		if (pid == -1)
+		if (pid < 0) // err handle
 		{
-			perror("fork");
-			return -1;
+			for (size_t j = 0; j < 2; j++) // in case it's the last arg
+				if (fd[j] != -1)
+					close(fd[j]);
+			return 1;
 		}
-
-		if (pid == 0) // Child
+		if (!pid) // child
 		{
-			if (handle_redirection(i, n, fds, tmp_fd) == -1)
-				exit(EXIT_FAILURE);
+			if (pfd) // should ignore the first case
+			{
+				if (dup2(pfd, 0) < 0)
+					exit(1);
+				close(pfd);
+			}
+			if (fd[1] != -1)
+			{
+				if (dup2(fd[1], 1) < 0)
+					exit(1);
+				close(fd[1]);
+				close(fd[0]); // close the fds either way
+			}
 			execvp(cmds[i][0], cmds[i]);
-			perror("execvp");
-			exit(EXIT_FAILURE);
+			exit(1); // exec fail
 		}
-		// Parent closes unused ends
-		if (tmp_fd != -1)
-			close(tmp_fd);      // Close previous read end
-		if (i < n - 1) {
-			tmp_fd = fds[0];    // Save current read end for next command
-			close(fds[1]);      // Close current write end
-		}
+		if (pfd)
+			close(pfd);
+		if (fd[1] != -1)
+			close(fd[1]);
+		pfd = fd[0];
 	}
-
-	// Wait for all children
-	for (int i = 0; i < n; i++)
-		wait(NULL);
+	while (wait(&stat) > 0) // wait for the program to finish
+	{
+		if (WIFEXITED(stat) && WEXITSTATUS(stat)) // if the exit sign is not 0 smth happened
+			return 1;
+		else if (!WIFEXITED(stat)) // the program did not terminated normally
+			return 1;
+	}
 	return 0;
-}
-
-int	calculate_cmds(char **cmds[], int *n)
-{
-	int	i = 0;
-
-	if (!cmds || ! *cmds[0] || !cmds[0][0])
-		return (1);
-	while (cmds[i] != NULL)
-	{
-		i++;
-	}
-	*n = i;
-	return (0);
-}
-
-int	picoshell(char **cmds[])
-{
-	int		n;
-
-	if (calculate_cmds(cmds, &n))
-		return (0);
-	#ifdef DEBUG
-		printf("Commands number %d\n", n);
-	#endif
-	if (n == 1)
-	{
-		if (execvp(cmds[0][0], cmds[0]) == -1)
-			return (1);
-		return (0);
-	}
-	else
-		if (execute_in_pipe(cmds, n) == -1)
-			return (1);
-	return (0);
 }
