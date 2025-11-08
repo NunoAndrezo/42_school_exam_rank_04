890edb42fd1ac33a2d049540a95bc53575e20fba
nuno <nunoandrezo97@gmail.com>
2025-11-08 18:08:33 +0000

    fix

--- lv01/sandbox/sandbox.c
+++ lv01/sandbox/sandbox.c
@@ -7,46 +7,48 @@
 #include <stdio.h>
 #include <signal.h>
 
-int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
+int	sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 {
-	int pid;
-	int ret;
-	int stat;
-	int es;
-	int sig;
+	pid_t	pid;
+	int		status;
+	int		exit_status;
+	int		signal;
 
 	pid = fork();
 	if (pid < 0)
 		return -1;
-	if (!pid)
+	if (pid == 0)
 	{
-		alarm(timeout);
+		if (timeout)
+			alarm(timeout);
 		f();
 		exit(0);
 	}
-	ret = waitpid(pid, &stat, 0);
-	if (ret < 0)
-		return -1;
-	if (WIFSIGNALED(stat))
+	waitpid(pid, &status, 0);
+	if (WIFSIGNALED(status))
 	{
-		sig = WTERMSIG(stat);
-		if (sig == SIGALRM)
+		signal = WTERMSIG(status);
+		if (signal == SIGALRM)
 		{
 			if (verbose) printf("Bad function: timed out after %u seconds\n", timeout);
 			return 0;
 		}
-		if (verbose) printf("Bad function: %s\n", strsignal(sig));
+		if (verbose) printf("Bad function: %s\n", strsignal(signal));
 		return 0;
-	}
-	if (WIFEXITED(stat))
+	if (WIFEXITED(status))
 	{
-		es = WEXITSTATUS(stat);
-		if (es)
+		exit_status = WEXITSTATUS(status);
+		if (exit_status == 0)
 		{
-			if (verbose) printf("Bad function: exited with code %u\n", es);
+			if (verbose) printf("Nice function!\n");
+			return 1;
+		}
+		else
+		{
+			if (verbose) printf("Bad function: exited with code %d\n", exit_status);
 			return 0;
 		}
-		if (verbose) printf("Nice function!\n");
 	}
-	return 1;
+	}
+	return (1);
 }
